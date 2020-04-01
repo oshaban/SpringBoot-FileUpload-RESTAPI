b29168552bf63cf85250c8449d7f057e594a7f01
Omar Shaban <omarshaban@Omars-MBP.fios-router.home>
2020-03-31 23:40:09 -0400

    Added commandlinerunner to setup storage service on init

--- src/main/java/com/oshaban/fileupload/FileUploadApplication.java
+++ src/main/java/com/oshaban/fileupload/FileUploadApplication.java
@@ -1,13 +1,24 @@
 package com.oshaban.fileupload;
 
+import com.oshaban.fileupload.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FileUploadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FileUploadApplication.class, args);
+    }
+
+    /**
+     * Runs once during application startup to init storageService
+     */
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return args -> storageService.init();
     }
 
 }
